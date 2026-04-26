b84aef5f599b8b525089aef6feeac30c0e5224e6
Christian Sailer <christian@blackseamonster.com>
2026-04-26 13:54:55 +0100

    Clean up warnings

--- src/rays/sections.rs
+++ src/rays/sections.rs
@@ -1,4 +1,3 @@
-use std::hash::Hash;
 use std::{collections::HashSet, fmt::Debug};
 use std::convert::From;
 
@@ -49,7 +48,7 @@ pub fn closest_section<'l>(ray: &Line, lines: &'l Vec<Line>) -> Option<Intersect
     let mut intersection: Option<Intersection> = None;
 
     for line in lines {
-        let mut i = section_point(ray, line);
+        let i = section_point(ray, line);
         if let Some(iv) = i {
             if intersection.is_none()
                 || iv.scale_factor < intersection.as_ref().unwrap().scale_factor
